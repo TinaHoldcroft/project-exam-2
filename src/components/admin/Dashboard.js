be6db9fe6943e136eabd679885137e4d27cc08fc
TinaHoldcroft <54176460+TinaHoldcroft@users.noreply.github.com>
2021-05-09 10:25:10 +0200

    flip-card

--- src/components/admin/Dashboard.js
+++ src/components/admin/Dashboard.js
@@ -10,13 +10,21 @@ function Dashboard() {
             <div className="dashboard">
                 <div class="card-group">
                     <div class="card">
-                        <img  class="logo" src="/visa.svg" alt="Visa"/>
-                        <img class="chip" src="/chip.svg" alt="chip"/>
-                        <div class="number">1234 5678 9012 3456</div>
-                        <div class="name">navn n navnesen</div>
-                        <p>valid <br/>thru</p>
-                        <div class="date">10/19</div>
-                        <div class="ring"></div>
+                        <div class="flip-card-inner">
+                            <div class="flip-card-front">
+                                <img  class="logo" src="/visa.svg" alt="Visa"/>
+                                <img class="chip" src="/chip.svg" alt="chip"/>
+                                <div class="number">1234 5678 9012 3456</div>
+                                <div class="name">navn n navnesen</div>
+                                <p>valid <br/>thru</p>
+                                <div class="date">10/19</div>
+                                <div class="ring"></div>
+                            </div>
+                            <div class="flip-card-back">
+                                <h5>Most used payment methods</h5>
+                                <div>[placeholder]</div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
